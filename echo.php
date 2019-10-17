a312e582a127972a5475a18d86c8393a2ca51868
mylab4492 <sadiquekhan449@gmail.com>
2019-10-17 11:50:03 +0530

    Update echo.php

--- echo.php
+++ echo.php
@@ -1,9 +1,30 @@
 <!DOCTYPE html>
 <html>
 <body>
-
 <?php
-echo "Hello world!";
+	// Account details
+	$apiKey = urlencode('iJdkVMsQp2Q-8rHxKRrGjbYrclbTWL1PN7zBjmUDmg');
+	
+	// Message details
+	$numbers = array(919167039216);
+	$sender = urlencode('TXTLCL');
+	$message = rawurlencode('This is your message');
+ 
+	$numbers = implode(',', $numbers);
+ 
+	// Prepare data for POST request
+	$data = array('apikey' => $apiKey, 'numbers' => $numbers, "sender" => $sender, "message" => $message);
+ 
+	// Send the POST request with cURL
+	$ch = curl_init('https://api.textlocal.in/send/');
+	curl_setopt($ch, CURLOPT_POST, true);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	$response = curl_exec($ch);
+	curl_close($ch);
+	
+	// Process your response here
+	echo $response;
 ?>
 
 </body>
