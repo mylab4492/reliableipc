3681fb2849476342b09a014a4d5e88301b849bae
mylab4492 <sadiquekhan449@gmail.com>
2019-10-20 11:14:06 +0530

    Update form2.php

--- form2.php
+++ form2.php
@@ -9,37 +9,8 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 
 <head>
 	<title>Enquiry Form</title>
-	<!-- Meta Tags -->
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<script type="application/x-javascript">
-		addEventListener("load", function () {
-			setTimeout(hideURLbar, 0);
-		}, false);
-
-		function hideURLbar() {
-			window.scrollTo(0, 1);
-		}
-	</script>
-	<!--// Meta Tags -->
-	<!-- Stylesheet -->
-	<!--// Stylesheet -->
-	<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" media="all">
-<!-- Slider -->
-<link rel="stylesheet" href="css/mainStyles.css" />
-<!-- //Slider -->
-<link href="css/easy-responsive-tabs.css" rel='stylesheet' type='text/css'/>
-<link href='css/simplelightbox.min.css' rel='stylesheet' type='text/css'> 
-<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" media="all">
-<link href="css/style.css" rel="stylesheet" type="text/css" media="all"/>
-	<link href="css/formstyle.css" rel='stylesheet' type='text/css' />
-<link href="//fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
-<link href="//fonts.googleapis.com/css?family=Raleway:400,500,700,800,900" rel="stylesheet">
-	<!--fonts-->
-	<link href="//fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700,800" rel="stylesheet">
-	<!--//fonts-->
 </head>
-
+	<title>Send mail Form</title>
 <body>
 
 		
