cc3bd2fea7fbe1339d2e7cd13103e9878a33c6dc
mylab4492 <sadiquekhan449@gmail.com>
2019-10-21 10:50:51 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -345,6 +345,10 @@
 
 <?php 
 error_reporting(E_ALL); ini_set('display_errors', '1');
+error_reporting(-1); // reports all errors
+ini_set("display_errors", "1"); // shows all errors
+ini_set("log_errors", 1);
+ini_set("error_log", "/tmp/php-error.log");
 if(isset($_POST['SubmitButton']))
 
 // Import PHPMailer classes into the global namespace
