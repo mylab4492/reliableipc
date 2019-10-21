676bc7e658773dfa6df280e23f1ec138b9a6f803
mylab4492 <sadiquekhan449@gmail.com>
2019-10-21 10:54:15 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -344,7 +344,6 @@
 </body>
 
 <?php 
-error_reporting(E_ALL); ini_set('display_errors', '1');
 error_reporting(-1); // reports all errors
 ini_set("display_errors", "1"); // shows all errors
 ini_set("log_errors", 1);
