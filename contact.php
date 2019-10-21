8c15ffafaef5f5b4ec6ddc22203de2a6439ada55
mylab4492 <sadiquekhan449@gmail.com>
2019-10-21 09:41:27 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -346,7 +346,7 @@
 </html>
 
 <?php 
-error_reporting(E_ALL); ini_set('display_errors', '1'); ini_set('html_errors', '1')
+error_reporting(E_ALL); ini_set('display_errors', '1');
 if(isset($_POST['SubmitButton']))
 
 // Import PHPMailer classes into the global namespace
@@ -355,10 +355,46 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
 {
-require ('php/Exception.php');
-require ('php/PHPMailer.php');
-require ('php/SMTP.php');
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
 // php
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 2;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+// Check if image file is a actual image or fake image
+// Check if file already exists
+if(!empty($_FILES["fileToUpload"]["name"])){
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 5000000) {
+echo '<script language="javascript">';
+echo 'alert("Sorry, your file is too large.")';
+echo '</script>';
+    $uploadOk = 0;
+}
+if ($uploadOk == 0) {
+echo '<script language="javascript">';
+echo 'history.go(-1)';
+echo '</script>';
+		die('Error: error');
+// if everything is ok, try to upload file
+} else {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        //echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+		$uploadOk = 1;
+    } else {
+		
+		echo '<script language="javascript">';
+        echo "Sorry, there was an error uploading your file.";
+		echo '</script>';
+echo '<script language="javascript">';
+echo 'history.go(-1)';
+echo '</script>';
+		die('Error: error');
+    }
+}
+}
 //msg
 
 	// Account details
@@ -448,7 +484,7 @@ try {
     $mail->Subject    = $subject;
     $mail->Body       = $message;
 if($mail->Send()) {
-	//unlink($target_file);
+	unlink($target_file);
 	mailSent = 1;
   echo "Mail sent!";
 } else {
